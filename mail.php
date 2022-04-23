ab1f6c1c2c97297e0a7ce17b1fe91399df482e24
mahfuz <mahfuz01bd@gmail.com>
2022-04-23 22:09:58 +0600

    send email via contact form

--- mail.php
+++ mail.php
@@ -1,47 +1,39 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer;
-use PHPMailer\SMTP;
-use PHPMailer\Exception;
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
+require './PHPMailer/src/Exception.php';
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+$mail = new PHPMailer\PHPMailer\PHPMailer();
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+//Variables
+$name = $_GET['name'];
+$to = $_GET['email'];
+$phoneNumber = $_GET['phone'];
+$subject = $_GET['subject'];
+$message = $_GET['message'];
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'thelionk91@gmail.com';                     //SMTP username
-    $mail->Password   = 'kfdqtefvumtvsgfg';                               //SMTP password
-    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-    $mail->Port       = 587; //465                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+//$mail->SMTPDebug = 3;                                 // Enable verbose debug output
 
-    //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+$mail->isSMTP();                                        // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';                         // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                                 // Enable SMTP authentication
+$mail->Username = 'thelionk91@gmail.com';               // SMTP username
+$mail->Password = 'wfvnlcxcgdpgcccc';                   // SMTP password
+$mail->SMTPSecure = 'tls';                              // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587; //25                                 // TCP port to connect to
 
-    //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+$mail->setFrom("Mr. Mango Express", "thelionk91@gmail.com");
+$mail->addAddress($to, $name . ' - ' . $phoneNumber);   // Add a recipient
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+$mail->isHTML(true);
+$mail->Subject = $subject;
+$mail->Body    = $message . '<br/><br/>==============================<br/> Email: ' . $to . '<br/> Name: ' . $name . '<br/>Phone Number: ' . $phoneNumber;
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+if(!$mail->send()) {
+    echo '<span class="loading text-danger text-left pl-4">';
+    echo 'Message could not be sent. <br/>';
+    echo 'Mailer Error: ' . $mail->ErrorInfo. '<a href="./contact.html">Try again</a>.';
+    echo '</span>';
+} else {
+    echo '<span class="loading text-success">Message have been sent.</span>';
 }
