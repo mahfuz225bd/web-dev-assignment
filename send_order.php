a4848b682e096cce4f7c543434322144e66b7ab3
mahfuz <mahfuz01bd@gmail.com>
2022-04-24 01:59:30 +0600

    sending order via email

--- send_order.php
+++ send_order.php
@@ -7,10 +7,28 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
 
 //Variables
 $name = $_GET['name'];
-$from = $_GET['email'];
+$email = $_GET['email'];
+$billingAddress = $_GET['billingAddress'];
 $phoneNumber = $_GET['phone'];
-$subject = $_GET['subject'];
-$message = $_GET['message'];
+$note = $_GET['note'];
+$shippingAddress = $_GET['shippingAddress'];
+$payment = $_GET['payment'];
+$orderDetails = $_GET['hiddenOrderDetails'];
+
+$message = sprintf("
+Name: %s <br/>
+Email: %s <br/>
+Billing Address: %s <br/>
+Phone Number: %s <br/>
+Say Something: %s <br/>
+====================================================================== <br/>
+Shipping Address: %s <br/>
+====================================================================== <br/>
+Payment Method: %s <br/>
+====================================================================== <br/>
+Order Details: <br/>
+%s
+", $name, $email, $billingAddress, $phoneNumber, $note, $shippingAddress, $payment, $orderDetails);
 
 //$mail->SMTPDebug = 3;                                 // Enable verbose debug output
 
@@ -22,18 +40,18 @@ $mail->Password = 'wfvnlcxcgdpgcccc';                   // SMTP password
 $mail->SMTPSecure = 'tls';                              // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587; //25                                 // TCP port to connect to
 
-$mail->setFrom($name, $from);
+$mail->setFrom($name, $email);
 $mail->addAddress("thelionk91@gmail.com");              // Add a recipient
 
 $mail->isHTML(true);
-$mail->Subject = $subject;
-$mail->Body    = $message . '<br/><br/>==============================<br/> Email: ' . $from . '<br/> Name: ' . $name . '<br/>Phone Number: ' . $phoneNumber;
+$mail->Subject = "A Order from " . $name . " - " . $phoneNumber;
+$mail->Body    = $message;
 
 if(!$mail->send()) {
     echo '<span class="loading text-danger text-left pl-4">';
-    echo 'Message could not be sent. <br/>';
-    echo 'Mailer Error: ' . $mail->ErrorInfo. '<a href="./contact.html">Try again</a>.';
+    echo 'Order could not be sent. <br/>';
+    echo 'Mailer Error: ' . $mail->ErrorInfo. '<a href="./checkout.html">Try again</a>.';
     echo '</span>';
 } else {
-    echo '<span class="loading text-success">Message have been sent.</span>';
+    echo '<span class="loading text-success">Order have been sent successfully.</span>';
 }
